6e3446e8d3fea7053c54369f18315fecdbd05ffb
Andrii-Stanislav <stanislav.andrii@gmail.com>
2021-02-16 18:11:55 +0200

    Finish test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+  На жаль, за день не зміг розібратися із RxJs, а раніше із цією бібліотекою не працював.
+  Буду далі в ній розбиратися, а Вам пропоную  мій варіант вирішення тестового завдання.
+*/
 
-function App() {
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './App.module.css';
+
+export default function App() {
+  const unixTime = 'Jan 01 1970 00:00:00 GMT+0300';
+  const [timer, setTimer] = useState(new Date(unixTime));
+  const [timerOn, setTimerOn] = useState(false);
+
+  const intervalId = useRef();
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  }, []);
+
+  const start = () => {
+    setTimerOn(true);
+    intervalId.current = setInterval(() => {
+      setTimer(prevTimer => new Date(prevTimer.getTime() + 1000));
+    }, 1000);
+  };
+
+  const stop = () => {
+    setTimerOn(false);
+    clearInterval(intervalId.current);
+    setTimer(new Date(unixTime));
+  };
+
+  const wait = () => {
+    setTimerOn(false);
+    clearInterval(intervalId.current);
+  };
+
+  const reset = () => {
+    stop();
+    start();
+  };
+
+  const doubleClick = useRef(false);
+  const heandleDoubleClick = () => {
+    if (doubleClick.current) {
+      wait();
+    }
+    doubleClick.current = !doubleClick.current;
+    setTimeout(() => {
+      doubleClick.current = false;
+    }, 300);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <p className={styles.clockface}>Таймер - {timer.toLocaleTimeString()}</p>
+      <div className={styles.buttonBox}>
+        <button
+          disabled={timerOn}
+          className={styles.button}
+          type="button"
+          onClick={start}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Start
+        </button>
+        <button
+          disabled={!timerOn}
+          className={styles.button}
+          type="button"
+          onClick={stop}
+        >
+          Stop
+        </button>
+        <button
+          disabled={!timerOn}
+          className={styles.button}
+          type="button"
+          onClick={heandleDoubleClick}
+        >
+          Wait
+        </button>
+        <button
+          disabled={!timerOn}
+          className={styles.button}
+          type="button"
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
+    </>
   );
 }
-
-export default App;
